987157af6c549a7f34c4b8dc2a6f014fb43e9370
SkyOPG <nicknameopg1@gmail.com>
2023-07-29 00:17:18 +0100

    100% c++

---
+++ src/gameFiles/data.cpp
@@ -0,0 +1,1 @@
+// g++ src/packages/discordinit.cpp src/gameFiles/gameScreens/vsplayer.cpp src/gameFiles/gameScreens/common.cpp src/packages/tstk.cpp src/assets/icons.o src/gameFiles/pong.cpp -o out/pong.exe -L./libs -I./includes -lraylib -lopengl32 -ldiscord-rpc -lgdi32 -lwinmm -Wall -Wextra -mwindows
